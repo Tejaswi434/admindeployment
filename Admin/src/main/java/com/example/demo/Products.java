27e1ef975260f109740ebddf0b3cec0ae0f286e5
Tejaswi434 <141810258+Tejaswi434@users.noreply.github.com>
2024-08-21 17:04:36 +0530

    Add files via upload

---
+++ Admin/src/main/java/com/example/demo/Products.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Products {
+   String name;
+   String description;
+   int price;
+   int category_id;
+   int vendor_id ;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public int getPrice() {
+	return price;
+}
+public void setPrice(int price) {
+	this.price = price;
+}
+public int getCategory_id() {
+	return category_id;
+}
+public void setCategory_id(int category_id) {
+	this.category_id = category_id;
+}
+public int getVendor_id() {
+	return vendor_id;
+}
+public void setVendor_id(int vendor_id) {
+	this.vendor_id = vendor_id;
+}
+   
+   
+}
